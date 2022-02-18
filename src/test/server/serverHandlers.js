b5850a4b665a3ad16f7346c8a8d195884193ae6b
Jordi-GV-Ikea <jordi.gonzalez.verges@ingka.com>
2022-02-18 23:05:37 +0100

    deleted pokemon

--- src/test/server/serverHandlers.js
+++ src/test/server/serverHandlers.js
@@ -1,18 +1,6 @@
 import { rest } from 'msw'
 
 const handlers = [
-  rest.get('https://pokeapi.co/api/v2/pokemon/ivysaur', (req, res, ctx) => {
-    const mockApiResponse = {
-      species: {
-        name: 'ivysaur',
-      },
-      sprites: {
-        front_shiny:
-          'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/2.png',
-      },
-    }
-    return res(ctx.json(mockApiResponse))
-  }),
   rest.get("https://isx-customer-services-pages-dppgxr4krq-ey.a.run.app/app-data?market=netherlands&environment=master", (req, res, ctx) => {
     const query = req.url.searchParams;
     const market = query.get("market");
